c642778c10d8a7859c37180240a25c25fce3e885
marvinpoehls <poehls.marvin@gmail.com>
2024-11-19 12:28:05 +0100

    Standard Form entfernt, Import Menu hinzugefügt, TextArea hinzugefügt zum anzeigen und editieren von Text, Button hinzugefügt zum importieren von Text-Dateien

---
+++ ImportMenu.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ParagonPioneers
+{
+    partial class ImportMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.submitImportButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(16, 76);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(767, 198);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(297, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Paragon Pioneers - Map Editor";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // submitImportButton
+            // 
+            this.submitImportButton.Location = new System.Drawing.Point(16, 369);
+            this.submitImportButton.Margin = new System.Windows.Forms.Padding(4);
+            this.submitImportButton.Name = "submitImportButton";
+            this.submitImportButton.Size = new System.Drawing.Size(100, 28);
+            this.submitImportButton.TabIndex = 2;
+            this.submitImportButton.Text = "Importieren";
+            this.submitImportButton.UseVisualStyleBackColor = true;
+            this.submitImportButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 282);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 28);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Datei importieren";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ImportMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 412);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.submitImportButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "ImportMenu";
+            this.Text = "Import Menu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button submitImportButton;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ImportMenu.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ParagonPioneers {
+    public partial class ImportMenu : Form {
+        public ImportMenu() {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //TODO Show Map Screen
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*",
+                Title = "Select a Text File"
+            };
+
+            // Show the dialog and check if a file is selected
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+
+                try
+                {
+                    // Read file content
+                    string fileContent = File.ReadAllText(filePath);
+
+                    // Display file content in a TextBox or any other control
+                    textBox1.Text = fileContent; // Assuming txtDisplay is a TextBox
+                }
+                catch (Exception ex)
+                {
+                    // Handle exceptions, e.g., file access issues
+                    MessageBox.Show($"Error reading file: {ex.Message}");
+                }
+            }
+        }
+    }
+}
