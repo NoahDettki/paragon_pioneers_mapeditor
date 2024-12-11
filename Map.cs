0fd0e0bf3cd895479ee8be72a7a7cad4ea044134
NoahDettki <122273050+NoahDettki@users.noreply.github.com>
2024-12-11 16:17:44 +0100

    feat: wip tiles check diagonal neighbours

--- Map.cs
+++ Map.cs
@@ -20,28 +20,37 @@ namespace ParagonPioneers
 
         private int selectedTile = 0;
 
+        private Image missingImage = Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "Missing.png"));
+
         private readonly Dictionary<int, Image[]> tileImages = new Dictionary<int, Image[]>() {
             [0] = new []
-            { 
+            {
+                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "Water____.png")),
+                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "WaterT___.png")),
+                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "Water_B__.png")),
+                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "WaterTB__.png")),
+                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "Water__L_.png")),
+                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "WaterT_L_.png")),
+                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "Water_BL_.png")),
+                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "WaterTBL_.png")),
+                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "Water___R.png")),
+                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "WaterT__R.png")),
+                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "Water_B_R.png")),
+                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "WaterTB_R.png")),
+                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "Water__LR.png")),
+                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "WaterT_LR.png")),
+                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "Water_BLR.png")),
                 Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "WaterTBLR.png")),
+                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "Water-TL.png")),
+                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "Water-TR.png")),
+                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "Water-BL.png")),
+                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "Water-BR.png")),
+
+
+
             },
             [1] = new []
             {
-                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "Land____.png")),
-                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "LandT___.png")),
-                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "Land_B__.png")),
-                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "LandTB__.png")),
-                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "Land__L_.png")),
-                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "LandT_L_.png")),
-                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "Land_BL_.png")),
-                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "LandTBL_.png")),
-                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "Land___R.png")),
-                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "LandT__R.png")),
-                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "Land_B_R.png")),
-                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "LandTB_R.png")),
-                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "Land__LR.png")),
-                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "LandT_LR.png")),
-                Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "Land_BLR.png")),
                 Image.FromFile(Path.Combine(Application.StartupPath, "../../Images", "LandTBLR.png")),
             },
             [2] = new []
@@ -103,33 +112,93 @@ namespace ParagonPioneers
                 for (int row = 0; row < rows; row++)
                 {
                     //int tileId = tiles[col, row];
-                    mapImages[col, row] = getDependendImage(col, row);
+                    mapImages[col, row] = GetDependendImage(col, row);
                 }
             }
             return mapImages;
         }
 
-        private Image getDependendImage(int x, int y) {
+        /// <summary>
+        /// Checks wether the specified coordinate is in the bounds of the tile map.
+        /// </summary>
+        /// <param name="x">the x coordinate</param>
+        /// <param name="y">the y coordinate</param>
+        /// <returns>true if inbound, else false</returns>
+        private bool IsInbounds(int x, int y) {
+            if (x < 0 || y < 0 || x >= tiles.GetLength(0) || y >= tiles.GetLength(1)) {
+                return false;
+            } else return true;
+        }
+
+        /// <summary>
+        /// Checks wether the type of the tile at the specified coordinate is the same type as the specified type
+        /// Out of bounds coordinates return true
+        /// </summary>
+        /// <param name="x">the x coordinate</param>
+        /// <param name="y">the y coordinate</param>
+        /// <param name="type">the tile type to compare with</param>
+        /// <returns></returns>
+        private bool IsTypeAt(int x, int y, int type) {
+            // Check for out of bounds
+            if (!IsInbounds(x, y)) return true;
+
+            // Compare type
+            return tiles[x, y] == type;
+        }
+
+        private Image GetDependendImage(int x, int y) {
+            if (!IsInbounds(x, y)) return missingImage;
+
             int type = tiles[x, y];
 
-            if (type == 0)
+            if (type != 0)
             {
                 return tileImages[type][0];
             }
 
-            // Get the surrounding tiles' types. Border is indicated by -1.
-            int typeTop = y > 0 ? tiles[x, y - 1] : -1;
-            int typeBottom = y < tiles.GetLength(1) - 1 ? tiles[x, y + 1] : -1;
-            int typeLeft = x > 0 ? tiles[x - 1, y] : -1;
-            int typeRight = x < tiles.GetLength(0) - 1 ? tiles[x + 1, y] : -1;
+            // Handling water tiles
+            if (type == 0) {
+                // Are the neighbouring tiles land tiles?
+                bool top = IsInbounds(x, y - 1) && tiles[x, y - 1] != 0;
+                bool bottom = IsInbounds(x, y + 1) && tiles[x, y + 1] != 0;
+                bool left = IsInbounds(x - 1, y) && tiles[x - 1, y] != 0;
+                bool right = IsInbounds(x + 1, y) && tiles[x + 1, y] != 0;
 
-            // Return image based on type and surrounding tiles
-            int index = 0;
-            index += typeTop == type ? 1 : 0;
-            index += typeBottom == type ? 2 : 0;
-            index += typeLeft == type ? 4 : 0;
-            index += typeRight == type ? 8 : 0;
-            return tileImages[type][index];
+                bool topLeft = IsInbounds(x - 1, y - 1) && tiles[x - 1, y - 1] != 0;
+                bool topRight = IsInbounds(x + 1, y - 1) && tiles[x + 1, y - 1] != 0;
+                bool bottomLeft = IsInbounds(x - 1, y + 1) && tiles[x - 1, y + 1] != 0;
+                bool bottomRight = IsInbounds(x + 1, y + 1) && tiles[x + 1, y + 1] != 0;
+
+                // Return water image based on neighbouring tiles
+                int index = 0;
+                index += top ? 1 : 0;
+                index += bottom ? 2 : 0;
+                index += left ? 4 : 0;
+                index += right ? 8 : 0;
+                
+                if (index == 0) {
+                    if (topLeft) return tileImages[type][16];
+                    if (topRight) return tileImages[type][17];
+                    if (bottomLeft) return tileImages[type][18];
+                    if (bottomRight) return tileImages[type][19];
+                }
+
+                return tileImages[type][index];
+            }
+
+            //// Get the surrounding tiles' types. Border is indicated by -1.
+            //int typeTop = y > 0 ? tiles[x, y - 1] : -1;
+            //int typeBottom = y < tiles.GetLength(1) - 1 ? tiles[x, y + 1] : -1;
+            //int typeLeft = x > 0 ? tiles[x - 1, y] : -1;
+            //int typeRight = x < tiles.GetLength(0) - 1 ? tiles[x + 1, y] : -1;
+
+            //// Return image based on type and surrounding tiles
+            //int index = 0;
+            //index += typeTop == type ? 1 : 0;
+            //index += typeBottom == type ? 2 : 0;
+            //index += typeLeft == type ? 4 : 0;
+            //index += typeRight == type ? 8 : 0;
+            return tileImages[type][0];
         }
 
         /// <summary>
@@ -146,22 +215,28 @@ namespace ParagonPioneers
                 
                 if (gridPos != null)
                 {
+                    int x = gridPos.Value.X;
+                    int y = gridPos.Value.Y;
+
+                    if (selectedTile == 0) {
+                        // Rules for water placement
+                        if (!IsTypeAt(x + 1, y, 0) && !IsTypeAt(x - 1, y, 0)) return;
+                        if (!IsTypeAt(x, y + 1, 0) && !IsTypeAt(x, y - 1, 0)) return;
+                    }
+
                     tiles[gridPos.Value.X, gridPos.Value.Y] = selectedTile;
-                    mapPanel.SetImageAt(getDependendImage(gridPos.Value.X, gridPos.Value.Y), gridPos.Value.X, gridPos.Value.Y);
+                    mapPanel.SetImageAt(GetDependendImage(x, y), x, y);
 
                     // Update surrounding tiles
-                    if (gridPos.Value.X > 0) {
-                        mapPanel.SetImageAt(getDependendImage(gridPos.Value.X - 1, gridPos.Value.Y), gridPos.Value.X - 1, gridPos.Value.Y);
-                    }
-                    if (gridPos.Value.X < tiles.GetLength(0) - 1) {
-                        mapPanel.SetImageAt(getDependendImage(gridPos.Value.X + 1, gridPos.Value.Y), gridPos.Value.X + 1, gridPos.Value.Y);
-                    }
-                    if (gridPos.Value.Y > 0) {
-                        mapPanel.SetImageAt(getDependendImage(gridPos.Value.X, gridPos.Value.Y - 1), gridPos.Value.X, gridPos.Value.Y - 1);
-                    }
-                    if (gridPos.Value.Y < tiles.GetLength(1) - 1) {
-                        mapPanel.SetImageAt(getDependendImage(gridPos.Value.X, gridPos.Value.Y + 1), gridPos.Value.X, gridPos.Value.Y + 1);
-                    }
+                    mapPanel.SetImageAt(GetDependendImage(gridPos.Value.X - 1, gridPos.Value.Y), gridPos.Value.X - 1, gridPos.Value.Y);
+                    mapPanel.SetImageAt(GetDependendImage(gridPos.Value.X + 1, gridPos.Value.Y), gridPos.Value.X + 1, gridPos.Value.Y);
+                    mapPanel.SetImageAt(GetDependendImage(gridPos.Value.X, gridPos.Value.Y - 1), gridPos.Value.X, gridPos.Value.Y - 1);
+                    mapPanel.SetImageAt(GetDependendImage(gridPos.Value.X, gridPos.Value.Y + 1), gridPos.Value.X, gridPos.Value.Y + 1);
+
+                    mapPanel.SetImageAt(GetDependendImage(gridPos.Value.X - 1, gridPos.Value.Y - 1), gridPos.Value.X - 1, gridPos.Value.Y - 1);
+                    mapPanel.SetImageAt(GetDependendImage(gridPos.Value.X - 1, gridPos.Value.Y + 1), gridPos.Value.X - 1, gridPos.Value.Y + 1);
+                    mapPanel.SetImageAt(GetDependendImage(gridPos.Value.X + 1, gridPos.Value.Y - 1), gridPos.Value.X + 1, gridPos.Value.Y - 1);
+                    mapPanel.SetImageAt(GetDependendImage(gridPos.Value.X + 1, gridPos.Value.Y + 1), gridPos.Value.X + 1, gridPos.Value.Y + 1);
                 }
             }
         }
